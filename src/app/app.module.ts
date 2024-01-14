e970df86d50912f69e6dc8df2eccd06b9da6af3b
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2024-01-14 20:44:54 +0700

    layout sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import {
   provideClientHydration,
 } from '@angular/platform-browser';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { TodoPageComponent } from './components/pages/todo-page/todo-page.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, SidebarComponent],
-  imports: [BrowserModule, AppRoutingModule, MatSidenavModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    SidebarComponent,
+    TodoPageComponent,
+    PagesComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
