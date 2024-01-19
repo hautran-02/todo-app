8af5c19abf78b9810b614c7c8323e1d59b81ffe2
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2024-01-19 16:07:46 +0700

    layout css: d-flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { TodoPageComponent } from './components/pages/todo-page/todo-page.compon
 import { PagesComponent } from './components/pages/pages.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,8 +31,10 @@ import { MatIconModule } from '@angular/material/icon';
     AppRoutingModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
